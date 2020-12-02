36ec058f45268bc3b5f04f373881f58bfd026d42
zhangyao <zhangyao@qq.com>
2020-12-02 22:23:28 +0800

    修改

--- git-test/src/com/itheima/git/FirstClass.java
+++ git-test/src/com/itheima/git/FirstClass.java
@@ -12,9 +12,11 @@ public class FirstClass {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        //hello test
     }
     public void test(){
         int i = 0;
         int j = 1;
+        System.out.println(i);
     }
 }
